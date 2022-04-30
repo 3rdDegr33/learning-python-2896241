a0348fb57c5139a79130d55f817b21378f4324f9
Johnny Moya <profjmoya@gmail.com>
2022-04-30 09:27:06 -0500

    Completed Chapter 4

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -9,11 +9,30 @@ def main():
     x, y = 10, 100
 
     # conditional flow uses if, elif, else
+    # if x < y:
+    #   result = "x is less than y"
+    # elif x == y:
+    #   result = "x is the same as y"
+    # else:
+    #   result = "x is greater than y"
+
+    # print(result)
 
     # conditional statements let you use "a if C else b"
+    # result = "x is less than y" if x < y else "x is greater or equal to y"
+    # print(result)
 
     # match-case makes it easy to compare multiple values
     value = "one"
+    # match value:    // Only works in Python 3.10+
+    #   case "one":
+    #     result = 1
+    #   case "two":
+    #     result = 2
+    #   case "three" | "four":
+    #     result = (3, 4)
+    #   case _:
+    #     result = -1
 
 if __name__ == "__main__":
     main()
